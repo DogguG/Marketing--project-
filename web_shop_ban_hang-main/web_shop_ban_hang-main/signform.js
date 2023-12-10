5d07a0315ee000d7887a9b3bd939c16105558adf
Trương Gia Thành <trggiathanh2003@gmail.com>
2023-12-10 14:21:28 +0700

    bugg css

--- web_shop_ban_hang-main/web_shop_ban_hang-main/signform.js
+++ web_shop_ban_hang-main/web_shop_ban_hang-main/signform.js
@@ -3,8 +3,8 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click",() =>{
-    container.classList.add(sign_up_mode);
+    container.classList.add("sign_up_mode");
 });
 sign_in_btn.addEventListener("click",() =>{
-    container.classList.remove(sign_in_mode);
+    container.classList.remove("sign_in_mode");
 });
